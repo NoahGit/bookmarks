28435e1c14093064cd6db3a45b5762278034a838
noah <gaoyong@xatu.edu.cn>
2020-02-25 23:43:59 +0800

    20200225-注册新用户自动增加profile并且编辑新用户的新增内容,所有老用户都手动增加了profile，-v2

--- account/views.py
+++ account/views.py
@@ -58,6 +58,7 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            return render(request, 'account/dashboard.html')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)

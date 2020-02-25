d0de932f33c025d0d9a3638e6d2fde7b2cd8cdea
noah <gaoyong@xatu.edu.cn>
2020-02-25 22:02:43 +0800

    20200225-数据库迁移之前，改好的扩展用户模型，修复错误-v2

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -156,4 +156,4 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'account/media/')

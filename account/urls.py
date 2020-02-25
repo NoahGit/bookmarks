0b53dac2d1075516782fd436f3a50e469b887842
noah <gaoyong@xatu.edu.cn>
2020-02-25 15:21:26 +0800

    20200225-添加注册页面-v1

--- account/urls.py
+++ account/urls.py
@@ -42,4 +42,5 @@ urlpatterns = [
              template_name="account/password_reset_complete.html"
          ),
          name='password_reset_complete'),
+    path('register/', views.register, name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -29,3 +29,19 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # 创建一个新的用户对象，但要避免保存它
+            new_user = user_form.save(commit=False)
+            # 设置选择的密码
+            new_user.set_password(user_form.cleaned_data['password'])
+            # 保存用户对象
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})

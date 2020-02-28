e3fd54ad4e694d4756e5ca81e5a5d678014ec96d
noah <gaoyong@xatu.edu.cn>
2020-02-28 21:21:29 +0800

    20200228-5.2.2覆写ModelForm的save()方法-v2

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'bookmarks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'account/templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

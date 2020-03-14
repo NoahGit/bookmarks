76f8abbc27635d2b39f7318c5cc26bae526dacdf
noah <gaoyong@xatu.edu.cn>
2020-03-15 00:06:23 +0800

    202003014-6.4.4将排名结果存储于数据库中-v1

--- images/views.py
+++ images/views.py
@@ -44,6 +44,8 @@ def image_detail(request, image_id, slug):
     image = get_object_or_404(Image, id=image_id, slug=slug)
     # increment total image views by 1增加总图像视图1
     total_views = r.incr('image:{}:views'.format(image.id))
+    # increment image ranking by 1增加图像排名1
+    r.zincrby('image_ranking', image.id, 1)
     return render(request, 'images/image/detail.html',
                   {'section': 'images', 'image': image, 'total_views': total_views})
 
@@ -84,3 +86,16 @@ def image_list(request):
                    {'section': 'images', 'page': page, 'images': images})
 
 
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(
+                           id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request,
+                  'images/image/ranking.html',
+                  {'section': 'images',
+                   'most_viewed': most_viewed})
